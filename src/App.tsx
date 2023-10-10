546b9b37f9a167de58e744be5c39f28d09526f50
Lucas <137804016+Lvcas2k4@users.noreply.github.com>
2023-10-10 11:21:46 -0300

    Login n Filter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+
+import {Login} from './Login.tsx';
+import { Filter } from './Filter.tsx';
+
+
 function App() {
   return (
     <>
-      <h1>Primer commit</h1>
+      {/* <h1>Primer commit</h1> */}
+{/* <Login /> */}
+<Filter/>
     </>
   );
 }
