855e212cfa7fad714ae52d07cdfb84013039cabb
Chunchunmaruinho <axelprogaymer33d@gmail.com>
2023-10-11 11:12:02 -0300

    ADD(pages): UserProfile

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,5 +1,22 @@
 import './ProfilePage.css'
 
 export const ProfilePage = () => {
-    return <div>MetricsPage</div>;
+
+
+    return (
+        <section className='profile-container'>
+            <div className='user-name-div'>
+                <p className="user-name">User Name</p>
+            </div>
+            <div className='black-div'>
+
+            </div>
+        </section>
+
+
+    )
+
+
 };
+
+
