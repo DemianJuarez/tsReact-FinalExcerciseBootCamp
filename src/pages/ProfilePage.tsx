dae3121ced0b0e0f6f76c31db8e1c2d03245b7ec
Chunchunmaruinho <axelprogaymer33d@gmail.com>
2023-10-11 13:05:17 -0300

    add(pages):profile page update

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -5,11 +5,35 @@ export const ProfilePage = () => {
 
     return (
         <section className='profile-container'>
-            <div className='user-name-div'>
-                <p className="user-name">User Name</p>
+            <div className='megadiv'>
+                <div className="user-profile-image">
+                    <img src="https://robohash.org/hicveldicta.png?size=50x50&set=set1" alt="" />
+                </div>
+                <div className='user-name-div'>
+                    <p className="user-name">User Name</p>
+                </div>
+                <div className='personal-info'>
+                    <div className="full-name">
+                        <p className='full-name-text'>Full Name</p>
+                        <p className="firstname">John</p>
+                        <p className="lastname">Biden</p>
+                    </div>
+                    <div className="email-div">
+                        <p className="email-text">Email</p>
+                        <p className='email'>god@gmail.com</p>
+                    </div>
+                    <div className="phone-div">
+                        <p className="phone-text">Phone</p>
+                        <p className="phone">12371923798</p>
+                    </div>
+                    <div className="address-div">
+                        <p className="address-text">Address</p>
+                        <p className="address">8374 anashe</p>
+                    </div>
+                </div>
             </div>
-            <div className='black-div'>
-
+            <div className="user-available-items">
+                <p>Buy history</p>
             </div>
         </section>
 
